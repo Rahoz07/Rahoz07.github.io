a9aea79ab431ebf31e14d49bc2f6515fa81ecb28
Zoharg_me <garciabrisa2007@gmail.com>
2024-10-07 13:43:03 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
     const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const password = document.getElementById('12345678').value;
 
-    if (username === 'admin' && password === 'password') {
+    if (username === 'admin' && password === '12345678') {
         alert('Inicio de sesión exitoso');
         // Redirigir a la página principal o dashboard
     } else {
